19513b841749571c5dd32bea48e9a321581a7829
Adolfo Reyes <adolfo0620@gmail.com>
2017-11-16 22:09:29 -0500

    adding login screen

---
+++ Smack/Smack/Contants.swift
@@ -0,0 +1,14 @@
+//
+//  Contants.swift
+//  Smack
+//
+//  Created by adolfo reyes on 11/16/17.
+//  Copyright © 2017 adolfo reyes. All rights reserved.
+//
+
+import Foundation
+
+
+
+// Segues
+let TO_LOGIN = "toLogin"
---
+++ Smack/Smack/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by adolfo reyes on 11/16/17.
+//  Copyright © 2017 adolfo reyes. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated:true, completion: nil)
+    }
+}

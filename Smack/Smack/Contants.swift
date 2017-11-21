9e9f0a9159f6343b674a057631b8f83235b550d3
Adolfo Reyes <adolfo0620@gmail.com>
2017-11-20 21:30:15 -0500

    users can now pick background colors for their icon also did some minor ui changes

--- Smack/Smack/Contants.swift
+++ Smack/Smack/Contants.swift
@@ -32,3 +32,9 @@ let USER_EMAIL = "userEmail"
 let HEADER = [
     "Content-Type": "application/json; charset=utf-8"
 ]
+
+// Notification Constants
+let NOTIF_USer_DATA_DID_CHANGE = Notification.Name("notifUserDataChanged")
+
+// Colors
+let smackPurplePlaceholser = #colorLiteral(red: 0.2588235294, green: 0.3294117647, blue: 0.7254901961, alpha: 0.5)

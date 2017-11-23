32f92f437381472dd626db5476b1cac81f2e8978
Adolfo Reyes <adolfo0620@gmail.com>
2017-11-23 01:31:47 -0500

    users can now create smack channels

---
+++ Smack/Smack/AddChannelVC.swift
@@ -0,0 +1,46 @@
+//
+//  AddChannelVC.swift
+//  Smack
+//
+//  Created by adolfo reyes on 11/23/17.
+//  Copyright © 2017 adolfo reyes. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+
+    @IBOutlet weak var nameTxt: UITextField!
+    
+    
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var chanDesc: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func closeModalPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+    
+    func setupView(){
+        let closeTouch = UIGestureRecognizer(target: self, action: #selector(AddChannelVC.closeeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSForegroundColorAttributeName: smackPurplePlaceholser])
+        chanDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSForegroundColorAttributeName: smackPurplePlaceholser])
+    }
+    @objc func closeeTap(_ recongnizer: UITapGestureRecognizer){
+        dismiss(animated: true, completion: nil)
+    }
+}
